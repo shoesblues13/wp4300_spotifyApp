4b490e79336d448cbd02dc53e5981737ae30eb88
loquence <ryandryv@gmail.com>
2017-04-23 12:19:32 -0400

    Add files via upload

--- term_project/src/persistlayer/ApolloPersistImpl.java
+++ term_project/src/persistlayer/ApolloPersistImpl.java
@@ -8,33 +8,15 @@ public class ApolloPersistImpl {
 
 	
 	public int addUser(User u){
-		byte[] salt = null;
-		String hpword = null;
-		try {
-			salt = PasswordHash.getSalt();
-			hpword = PasswordHash.hashPass(u.getPassword(), salt);
-		} catch (NoSuchAlgorithmException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 		
-		String sql = "INSERT INTO users (first_name,last_name,email,username,password,salt) VALUES" + "('" +u.getFname()+"','" + u.getLname() +"','"+u.getEmail()+ "','"+ u.getUsername() + "','"+ u.getPassword() + "','" +salt +"');" ;;
+		
+		String sql = "INSERT INTO users (first_name,last_name,email,username,password,salt) VALUES" + "('" +u.getFname()+"','" + u.getLname() +"','"+u.getEmail()+ "','"+ u.getUsername() + "','"+ u.getPassword() + "');" ;;
 		return DbAccessImpl.create(sql);
 	}
 	
 	public int signIn(String uname, String pword){
 		String sql = "SELECT password FROM users WHERE username=\"" + uname + "\";";
-		String sql2 = "SELECT salt FROM users where username=\"" + uname+"\";";
 		String r = DbAccessImpl.getString(sql, "password");
-		byte[] s = DbAccessImpl.getBytes(sql2, "salt");
-		
-		String hpword = null;
-		try {
-			hpword = PasswordHash.hashPass(pword, s);
-		} catch (NoSuchAlgorithmException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 		if (pword.equals(r))
 			return 1;
 		else 
@@ -47,6 +29,11 @@ public class ApolloPersistImpl {
 		String r = DbAccessImpl.getString(sql, "first_name");
 		return r;
 		
+	}
+	
+	public int addParty(Party p) {
+		String sql = "INSTER into party  (name,stime,etime,description,location,public,score,status) VALUES('"+p.getName()+"','"+p.getStime()+"','"+p.getEtime()+"','"+p.getDescription()+"','"+p.getLocation()+"','"+p.getPub()+"','"+p.getScore()+"','"+p.getStatus() + "');"; 
+		return DbAccessImpl.create(sql);
 	}
 	
 }
